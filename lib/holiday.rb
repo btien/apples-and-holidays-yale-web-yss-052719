ce5d66ec07afbb162542a342f8334ce4711d5baf
Ben Tien <benjamin.tien@flatironschool.com>
2019-01-06 21:39:35 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -66,13 +66,15 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  bbq_array = []
   holiday_hash.each do |holiday_season, holiday|
      holiday.each do |holiday, supplies|
         if(supplies.include?("BBQ"))
-          puts holiday
+          bbq_array << holiday
         end
       end
     end
+    bbq_array
 end
 
 
